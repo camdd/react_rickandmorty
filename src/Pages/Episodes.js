203e88c81c08dd2ecad58802eaea66630de659b4
Cam <camila.dambrosio95@gmail.com>
2022-10-07 13:53:07 -0300

    cardDetails added

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
-import Card from "../components/Card/Card";
-import InputGroup from "../components/Filter/category/InputGroup";
+import React, { useEffect, useState } from "react"
+import Card from "../components/Card/Card"
+import InputGroup from "../components/Filter/category/InputGroup"
 
 const Episodes = () => {
-    let [results, setResults] = React.useState([]);
-    let [info, setInfo] = useState([]);
-    let { air_date, episode, name } = info;
-    let [id, setID] = useState(1);
+    let [results, setResults] = React.useState([])
+    let [info, setInfo] = useState([])
+    let { air_date, episode, name } = info
+    let [id, setID] = useState(1)
   
-    let api = `https://rickandmortyapi.com/api/episode/${id}`;
+    let api = `https://rickandmortyapi.com/api/episode/${id}`
 
     useEffect(() => {
         (async function () {
-          let data = await fetch(api).then((res) => res.json());
-          setInfo(data);
+          let data = await fetch(api).then((res) => res.json())
+          setInfo(data)
       
           let a = await Promise.all(
             data.characters.map((x) => {
-              return fetch(x).then((res) => res.json());
+              return fetch(x).then((res) => res.json())
             })
-          );
-          setResults(a);
-        })();
-    }, [api]);
+          )
+          setResults(a)
+        })()
+    }, [api])
 
     return (
         <div className="container">
